6627ab8a531baa1f4a72e75d860ceab5fbd49015
Mihail <diakudza@gmail.com>
2022-01-31 19:46:39 +0300

    lesson5

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
+
 
 class MainController extends Controller
 {
     public function index()
     {
+
         return view('admin.index');
     }
 }
