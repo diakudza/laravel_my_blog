8f716236cf47157500f8b91c5945de2f3714d4fe
Mihail <diakudza@gmail.com>
2022-02-01 14:51:55 +0300

    lesson8

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags=Tag::paginate(10);
+        return view('admin.tags.index',compact('tags'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        return view('admin.tags.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required',
+        ]);
+
+        Tag::create($request->all());
+        //$request->session()->flash('success','Категория добавлена');
+        return redirect()->route('tags.index')->with('success','Категория добавлена');
+    }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tag = Tag::find($id);
+
+        return view('admin.tags.edit',compact('tag'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $request->validate([
+            'title'=>'required',
+        ]);
+
+        $tag = Tag::find($id);
+        $oldTag=$tag->title;
+//        $tag->slug = null;
+        $tag->update($request->all());
+        return redirect()->route('tags.index')->with('success',"Тэг id:{$tag->id} \"{$oldTag}\" переименован в \"{$tag->title}\" ");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $tag = Tag::find($id);
+        $tag->delete();
+        return redirect()->route('tags.index')->with('success',"Тэг id:{$tag->id}  \"{$tag->title}\" удален");
+    }
+}
